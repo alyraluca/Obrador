b88e16842cd03f298b487732a7e361825d7b82fe
alyraluca <alex@UbuntuAlex>
2024-01-19 22:51:32 +0100

    100

--- __manifest__.py
+++ __manifest__.py
@@ -2,16 +2,25 @@
 {
     'name': "produccion_mod2",
 
-    'summary': """
+    'summary': "Creación de recetas""
         Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
 
-    'description': """
+    'description': "Creación de recetas, lista de alergenos presentes en los productos y sobras de alimentos del dia no vendidos.""
         Long description of module's purpose
-    """,
+    "Creación de recetas a partir de nuestro inventario de materia prima comprada.
+Cada producto final creado tendrá una lista de alergenos.
+Lista de sobras por dia, semana, mes. De está manera se podrá mejorar la producción, reducir las sobras y ahorrar recursos y materia prima. TAB (Recetas)
+Lista de productos de creación propia organizados por: categoria.
+Botton de crear receta/producto: o Ingredientes (se puede escoger de la lista de materia prima que tendremos almacenado en los otros modulos.) o Cantidad: cada ingrediente tendrá la cantidad especificada TAB (Alergenos)
+Lista alergenos
+Botton NUEVO alergeno: donde podremos añadir nuevos alergenos.
+Tenemos una vista por lista de alergenos y otra vista por tabla de alergenos junto con los productos TAB(Sobras)
+Vemos una lista (que se puede organizar por dia, semana o mes) con las sobras.
+Hay in botton de NUEVO, donde se puede poner las sobras de ese día de alimentos perecederos."",
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Alexandra Raluca, Savu",
+    'website': "https://github.com/alyraluca/produccion_mod2",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
