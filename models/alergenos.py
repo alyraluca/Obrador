c55cbf73af4c88b0ce75549890858c18cfb1d41a
alyraluca <alex@UbuntuAlex>
2025-01-28 12:27:37 +0000

    Fix error lista alergenos

--- models/alergenos.py
+++ models/alergenos.py
@@ -8,11 +8,6 @@ class Alergenos(models.Model):
     recetas_count  = fields.Integer(string='Número de recetas', compute = '_compute_recetas_count')
     #recetas_tags = fields.Char(string = "Recetas con este alérgeno", compute = '_compute_recetas_tags')
 
-    #Contar todas las recetas que tienen ese alergeno
-    @api.depends('recetas_ids')
-    def _compute_recetas_count(self):
-        for record in self:
-            record.recetas_count = len(record.recetas_ids)
     
     #Relación Many2many con las recetas
     recetas_ids = fields.Many2many(
@@ -22,11 +17,16 @@ class Alergenos(models.Model):
         'receta_id',
         string = 'Recetas'
     )
+    #Contar todas las recetas que tienen ese alergeno
+    @api.depends('recetas_ids')
+    def _compute_recetas_count(self):
+        for record in self:
+            record.recetas_count = len(record.recetas_ids)
 
     #Obtener los nombres de las recetas
-    
-    #def _compute_recetas_tags(self):
-     #   for record in self:
-      #      recetas_names = [receta.name for receta in record.recetas_ids]
-       #     record.recetas_tags = ', '.join(recetas_names) if recetas_names else ''
+    """ @api.depends('recetas_ids')
+    def _compute_recetas_tags(self):
+        for record in self:
+            recetas_names = [receta.name for receta in record.recetas_ids]
+            record.recetas_tags = ', '.join(recetas_names) if recetas_names else '' """
             
