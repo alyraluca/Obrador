40c824d813f3de49d498e3e9a3ee6b5f43c0a50a
alyraluca <alex@UbuntuAlex>
2025-01-28 11:45:58 +0000

    Añadir tags de rtas en alergenos

--- models/alergenos.py
+++ models/alergenos.py
@@ -5,9 +5,10 @@ class Alergenos(models.Model):
     _description = 'Alergenos'
 
     name = fields.Char(string='Nombre')
-
     recetas_count  = fields.Integer(string='Número de recetas', compute = '_compute_recetas_count')
+    recetas_tags = fields.Char(string = "Recetas con este alérgeno", compute = "_compute_recetas_tags")
 
+    #Contar todas las recetas que tienen ese alergeno
     @api.depends('recetas_ids')
     def _compute_recetas_count(self):
         for record in self:
@@ -21,3 +22,10 @@ class Alergenos(models.Model):
         'receta_id',
         string = 'Recetas'
     )
+
+    #Obtener los nombres de las recetas
+
+    def _compute_recetas_tags(self):
+        for record in self:
+            recetas_names = [receta.name for receta in record.recetas_ids]
+            record.recetas_tags = ', '.join(recetas_names) if recetas_names else ''
